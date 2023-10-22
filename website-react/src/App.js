750a2b639d02468062c3676606f2d38f5281e388
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-10-22 22:42:34 +0200

    refactor(head): move html tags of `head` form `components/Head.js` to `public/index.html`
    References:
    - https://github.com/rascui/website/issues/28

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,12 +1,10 @@
 import './assets/css/style.css';
-import Head from './components/Head';
 import HeaderNav from './components/HeaderNav';
 import SectionHome from './components/SectionHome';
 
 function App() {
   return (
     <div className="App">
-      <Head />
       <header className="App-header">
         <HeaderNav />
       </header>
