b4f70fb52758f2d20b6d96af3535868bb97262a6
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-10-22 21:26:15 +0200

    feat(header): create `HeaderNav` component
    References:
    - https://github.com/rascui/website/issues/28

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import './assets/css/style.css';
+import HeaderNav from './components/HeaderNav';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <HeaderNav />
       </header>
     </div>
   );
