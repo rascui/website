08d60bd43874fe2d1042f65148ac8bf70816d2d9
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-10-22 22:32:17 +0200

    refactor(react): remove default structure
    References:
    - https://github.com/rascui/website/issues/28

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import './assets/css/style.css';
 import Head from './components/Head';
 import HeaderNav from './components/HeaderNav';
@@ -10,18 +8,6 @@ function App() {
     <div className="App">
       <Head />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <HeaderNav />
       </header>
       <SectionHome />
